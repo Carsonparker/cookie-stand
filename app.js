6f36ae4313f00f1050c61110fbd9c8e6512bc4fe
Carson Daniel Parker <Carson@dkparker.com>
2015-11-02 14:56:38 -0800

    Calculate number of cookies purchased per hour works. Also, storred results for location in its own array and displayed in in my HTML file.

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
+var hours = ['10 am', '11 am', 'Noon', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm'];
+
+
+
 var pikePlace = {
   minCustHr: 17,
   maxCustHr: 88,
   avgCookies: 5.2,
+  hourlySales: [],
 
   calcRandCust: function() {
     return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
   },
   calcHourlySales: function() {
-    return this.avgCookies * this.calcRandCust();
+    return Math.floor(this.avgCookies * this.calcRandCust());
   },
+
 };
-console.log(pikePlace.calcRandCust(pikePlace.minCustHr, pikePlace.maxCustHr))
+  for (var i = 0; i < hours.length; i++) {
+    pikePlace.hourlySales.push(pikePlace.calcHourlySales())
+  }
+  for (var i = 0; i < pikePlace.hourlySales.length; i++) {
+    var node = document.createElement('li')
+    var textnode = document.createTextNode(hours[i] + ': ' + pikePlace.hourlySales[i])
+    node.appendChild(textnode);
+    document.getElementById('list').appendChild(node);
+  }
+  console.log(pikePlace.calcRandCust(pikePlace.minCustHr, pikePlace.maxCustHr))
 
 
 
